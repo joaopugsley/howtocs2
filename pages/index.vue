728594240d7f68683976b814358f964e28fc95a6
joaopugsley <joaopugsleyy@gmail.com>
2024-03-27 23:05:58 -0300

    feat: adjust page size for better readability on tablets

--- pages/index.vue
+++ pages/index.vue
@@ -34,7 +34,7 @@ useHead({
 </script>
 
 <template>
-  <section class="w-full lg:w-3/5 flex flex-col space-y-4 md:space-y-8">
+  <section class="w-full xl:w-3/5 flex flex-col space-y-4 md:space-y-8">
     <MainArticle/>
     <ArticleWrapper/>
   </section>
