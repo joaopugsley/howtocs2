0cd8ce573cd0add20231188691b4e915382d4fcc
joaopugsley <joaopugsleyy@gmail.com>
2024-03-27 23:15:29 -0300

    feat: adjustable twitter embed height

--- components/content/TwitterEmbed.vue
+++ components/content/TwitterEmbed.vue
@@ -1,20 +1,19 @@
 <script setup lang="ts">
-  const { id } = defineProps({
+  const { id, height } = defineProps({
     id: {
       type: String,
       required: true,
+    },
+    height: {
+      type: Number,
+      required: false,
+      default: 650
     }
   })
 </script>
 
 <template>
   <div class="w-full h-fit flex justify-center items-center">
-    <iframe width="550" height="650" scrolling="no" allowfullscreen :src="`https://platform.twitter.com/embed/Tweet.html?dnt=true&embedId=twitter-widget-0&frame=false&hideCard=false&hideThread=true&id=${id}&lang=pt&theme=dark&widgetsVersion=82e1070%3A1619632193066&width=550px`"/>
+    <iframe width="550" :height="height" scrolling="no" allowfullscreen :src="`https://platform.twitter.com/embed/Tweet.html?dnt=true&embedId=twitter-widget-0&frame=false&hideCard=false&hideThread=true&id=${id}&lang=pt&theme=dark&widgetsVersion=82e1070%3A1619632193066&width=550px`"/>
   </div>
 </template>
-
-<style>
-  iframe {
-    min-height: 650px;
-  }
-</style>
