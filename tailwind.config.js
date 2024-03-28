a1c89cddcf8e09747bebf20a059bf5ef2af7c8c5
joaopugsley <joaopugsleyy@gmail.com>
2024-03-28 04:53:28 -0300

    feat: create zinc-850 color on tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        zinc: {
+          850: "#202023"
+        }
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
