8dd3b55541b18317c44145b8c96544e0437e6615
joaopugsley <joaopugsleyy@gmail.com>
2024-03-31 18:55:03 -0300

    feat: create new content component 'BlogImage'

---
+++ components/content/BlogImage.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+  const { src, alt } = defineProps({
+    src: {
+      type: String,
+      required: true,
+    },
+    alt: {
+      type: String,
+      required: true,
+    },
+  })
+</script>
+
+<template>
+  <div class="relative w-full h-96 flex justify-center items-center overflow-hidden p-0">
+    <span v-if="alt" class="absolute right-2 bottom-2 text-xs sm:text-sm px-2 py-1 text-indigo-400 bg-zinc-900 rounded-md font-bold select-all"><span class="select-none">Foto: </span>{{ alt }}</span>
+    <img :src="src" class="w-full object-cover" :alt="alt"/>
+  </div>
+</template>
