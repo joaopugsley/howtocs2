8bb0fa37b0b400d1b04a5ac02e526245ed5d6195
joaopugsley <joaopugsleyy@gmail.com>
2024-03-31 04:53:50 -0300

    feat: SEO optimization

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+
+const jsonld = {"@context": "https://schema.org", "@graph": [{"@type": "WebPage","@id": "https://howtocs2.com/","url": "https://howtocs2.com/","name": "HOWTOCS2.com / Seu portal de notícias sobre o Counter Strike brasileiro. Fique por dentro das últimas novidades, análises, eventos e muito mais.","isPartOf": {  "@id": "https://howtocs2.com/"},"about": {  "@id": "https://howtocs2.com/"},"primaryImageOfPage": {  "@id": "https://howtocs2.com/"},"image": {  "@id": "https://howtocs2.com/"},"datePublished": "2023-01-01T18:00:00+00:00","dateModified": new Date().toISOString(),"description": "HOWTOCS2.com / Seu portal de notícias sobre o Counter Strike brasileiro. Fique por dentro das últimas novidades, análises, eventos e muito mais.","breadcrumb": {  "@id": "https://howtocs2.com/"},"inLanguage": "pt-BR","potentialAction": [  {    "@type": "ReadAction",    "target": [      "https://howtocs2.com/"    ]  }]},{"@type": "BreadcrumbList","@id": "https://howtocs2.com/","itemListElement": [  {    "@type": "ListItem",    "position": 1,    "name": "Home"  }]},{"@type": "WebSite","@id": "https://howtocs2.com/","url": "https://howtocs2.com/","name": "HOWTOCS2","description": "Seu portal de notícias sobre o Counter Strike brasileiro. Fique por dentro das últimas novidades, análises, eventos e muito mais.","publisher": {  "@id": "https://howtocs2.com/"},"inLanguage": "pt-BR"},{"@type": "Organization","@id": "https://howtocs2.com/","name": "HOWTOCS2","url": "https://howtocs2.com/","sameAs": [  "https://twitter.com/howtocs2"]}]};
+
 useHead({
   title: "HOWTOCS2.com / Seu portal de notícias sobre o Counter Strike brasileiro.",
   htmlAttrs: {
@@ -24,6 +27,13 @@ useHead({
     {
       name: 'language',
       content: 'Portuguese'
+    }
+  ],
+  script: [
+    {
+      hid: "app-json-ld",
+      type: "application/ld+json",
+      textContent: JSON.stringify(jsonld)
     }
   ]
 });
